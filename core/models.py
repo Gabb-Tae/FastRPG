b336c434de66184567ce349b2b27597c2ccb0855
Gabriel <gabbmaia2004@gmail.com>
2022-09-12 10:50:26 -0300

    models adicionais

--- core/models.py
+++ core/models.py
@@ -6,8 +6,30 @@ class rpgclass(models.Model):
     life = models.IntegerField(max_length=5)
     main_skill = models.CharField(max_length=20)
     strength = models.IntegerField(max_length=2)
-    des = models.IntegerField(max_length=2)
-    con = models.IntegerField(max_length=2)
-    int = models.IntegerField(max_length=2)
+    dexterity = models.IntegerField(max_length=2)
+    constitution = models.IntegerField(max_length=2)
+    intelligence = models.IntegerField(max_length=2)
     wisdom = models.IntegerField(max_length=2)
     charism = models.IntegerField(max_length=2)
+
+class skills(models.Model):
+    description = models.CharField(max_length=1000)
+
+class itens(models.Model):
+    description = models.CharField(max_length=100)
+    damage = models.CharField(max_length=50)
+
+class rpgrace(models.Model):
+    name = models.CharField(max_length=50)
+    features = models.CharField(max_length=700)
+    life = models.IntegerField(max_length=5)
+    size = models.CharField(max_length=20)
+    speed = models.CharField(max_length=20)
+    languages = models.CharField(max_length=60)
+    strength = models.IntegerField(max_length=2)
+    dexterity = models.IntegerField(max_length=2)
+    constitution = models.IntegerField(max_length=2)
+    intelligence = models.IntegerField(max_length=2)
+    wisdom = models.IntegerField(max_length=2)
+    charism = models.IntegerField(max_length=2)
+
